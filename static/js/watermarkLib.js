e369d57be06814f29cc20cc4fb0f654a641ec54b
5ia15adbuechi <adrianbuechi@gmail.com>
2018-05-28 14:51:43 +0200

    updated competences and library

--- static/js/watermarkLib.js
+++ static/js/watermarkLib.js
@@ -1,4 +1,4 @@
-function uploadImage() {
+function watermarkImage() {
     const upload = document.querySelector('input[type=file]').files[0];
     watermark([upload, '/static/images/watermark_template.png'])
         .image(watermark.image.lowerLeft(0.5))
